4fc519f517bcd1aa465f9b5081cea3c18904bb29
Chris Guarino <chris.francis.guarino@gmail.com>
2025-01-07 20:33:13 -0500

    Added the key back in

--- command_parsing.py
+++ command_parsing.py
@@ -1,7 +1,7 @@
 import openai
 import json
 
-openai.api_key = 
+openai.api_key = "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"
 
 def parse_command(command):
     """
